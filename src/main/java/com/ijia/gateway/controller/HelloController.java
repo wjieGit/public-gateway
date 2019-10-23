3aeeacd2dd0a56a0898add956e258804c72c48f5
wjie <1215334983@qq.com>
2019-10-23 14:27:58 +0800

    gateway初始化

---
+++ src/main/java/com/ijia/gateway/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.ijia.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("hello")
+    public String Hello(){
+        return "Welcome to Gateway";
+    }
+}
