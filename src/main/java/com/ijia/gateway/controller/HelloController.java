94e19aec5fa28f6a069eff519ebd13f49d101c3d
wjie <1215334983@qq.com>
2019-10-24 14:16:13 +0800

    gateway2

--- src/main/java/com/ijia/gateway/controller/HelloController.java
+++ src/main/java/com/ijia/gateway/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("hello")
     public String Hello(){
-        return "Welcome to Gateway";
+        return "Welcome to Gateway2";
     }
 
 
