5faa80176ea2226c43a3a93b289246999f7562b4
wjie <1215334983@qq.com>
2019-11-08 17:23:39 +0800

    整合swagger2

---
+++ src/main/java/com/ijia/gateway/controller/SwaggerController.java
@@ -0,0 +1,35 @@
+package com.ijia.gateway.controller;
+
+import com.ijia.gateway.config.SwaggerProvider;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger.web.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/swagger-resources")
+public class SwaggerController {
+
+    @Resource
+    private SwaggerProvider swaggerProvider;
+
+
+    @RequestMapping(value = "/configuration/security")
+    public ResponseEntity<SecurityConfiguration> securityConfiguration() {
+        return new ResponseEntity<>(SecurityConfigurationBuilder.builder().build(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/configuration/ui")
+    public ResponseEntity<UiConfiguration> uiConfiguration() {
+        return new ResponseEntity<>(UiConfigurationBuilder.builder().build(), HttpStatus.OK);
+    }
+
+    @RequestMapping
+    public ResponseEntity<List<SwaggerResource>> swaggerResources() {
+        return new ResponseEntity<>(swaggerProvider.get(), HttpStatus.OK);
+    }
+}
